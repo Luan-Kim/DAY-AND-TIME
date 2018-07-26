dce15804c8da94b6cdd03b3460955fae2ff58d98
Kim kyoung min <36768658+Luan-Kim@users.noreply.github.com>
2018-07-26 23:37:05 +0900

    easy print day

---
+++ Print-Day.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	printf("Today is Sunday\n");
+	printf("Today is Monthday\n");
+	printf("Today is Tuesday\n");
+	printf("Today is Wednesday\n");
+	printf("Today is Thursday\n");
+	printf("Today is Saturday\n");
+}
